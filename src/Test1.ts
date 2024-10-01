d6d71bcf47f401cc456aadf57128da22d5ffb979
Panda-215 <151113211+Panda-215@users.noreply.github.com>
2024-10-01 13:28:26 +0700

    done

---
+++ src/Test1.ts
@@ -0,0 +1,7 @@
+import { Utils } from "./Utils";
+
+if (Utils.add(1, 2) === 6) {
+    console.log(0)
+} else {
+    console.log(1)
+}
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-async function addUser(data: any) {
+function add(a: number, b: number) {
+    return a + b;
+}
+
+async function addUser(data:any) {
     const response: any = await axios.post('https://jsonplaceholder.typicode.com/users', data);
    console.log (response);
    return response.data
 
 }
-export const Utils = { addUser };
+export const Utils = { addUser, add }
